8e18c052b38205311e0a1191fb14097479d51fc9
Carolina Amadio <33064803+CarolinaAmadio@users.noreply.github.com>
2024-06-17 17:26:24 +0200

    Update download_data_copernicus.py
    add info on the domain

--- Download/download_data_copernicus.py
+++ Download/download_data_copernicus.py
@@ -1,6 +1,8 @@
 import copernicusmarine
 
 #INFO:  https://help.marine.copernicus.eu/en/articles/7949409-copernicus-marine-toolbox-introduction 
+# Global contain all data but copernicusmarine.get doesnt filter per lat -lon  
+
 
 gl_dataset = copernicusmarine.get(
     dataset_id = 'cmems_obs-ins_ibi_phybgcwav_mynrt_na_irr' ,
@@ -8,4 +10,4 @@ gl_dataset = copernicusmarine.get(
     username = 'camadio' ,
     password = 'Bsqx3066!!' ,
     filter = "GL_*_GL*"
-    )
+    ) 
