b6934153d68f41adfc8b840bb8157cbac626be48
CarolinaAmadio <caio.spoleto@gmail.com>
2024-07-15 12:16:35 +0200

    controlled the printing

--- Main_Cmems_superglider.sh
+++ Main_Cmems_superglider.sh
@@ -11,11 +11,22 @@ DATE=20240616
 source /g100_work/OGS23_PRACE_IT/COPERNICUS/sequence3.sh
 
 #copernicusmarine login -username 'camadio' -password 'Bsqx3066!!'
-rm -r *.txt
+# Check if there are .txt files in the current directory
+if ls *.txt 1> /dev/null 2>&1; then
+    # Remove all .txt files
+    rm *.txt
+    echo "Removed all .txt files"
+else
+    echo "No .txt files found and removed before downloading new data"
+fi
 mkdir -p SUPERGLIDER
 
 #1 -->  download  files index.txt 
 copernicusmarine get -i cmems_obs-ins_ibi_phybgcwav_mynrt_na_irr --index-parts -nd --force-download
+
+rm -r index_history.txt index_monthly.txt index_platform.txt
+exit 0
+
 
 #2 --> filter the data per platform
 grep -E "*GL_PR_GL*" index_latest.txt | cut -d ',' -f 2 | rev | cut -d '/' -f 1,2 | rev > files_to_download_all.txt
